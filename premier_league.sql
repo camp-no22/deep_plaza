898d6198b43945dd0ce66f0ed123c7fa9b768211
KQU912 <116973256+KQU912@users.noreply.github.com>
2024-08-05 15:00:23 +0900

    Update premier_league.sql

--- premier_league.sql
+++ premier_league.sql
@@ -14,6 +14,90 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `coaches`
+--
+
+DROP TABLE IF EXISTS `coaches`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `coaches` (
+  `coach_id` int NOT NULL AUTO_INCREMENT,
+  `team_id` int DEFAULT NULL,
+  `name` varchar(100) NOT NULL,
+  `nationality` varchar(50) DEFAULT NULL,
+  `birthdate` date DEFAULT NULL,
+  PRIMARY KEY (`coach_id`),
+  KEY `team_id` (`team_id`),
+  CONSTRAINT `coaches_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `teams` (`team_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `coaches`
+--
+
+LOCK TABLES `coaches` WRITE;
+/*!40000 ALTER TABLE `coaches` DISABLE KEYS */;
+/*!40000 ALTER TABLE `coaches` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `players`
+--
+
+DROP TABLE IF EXISTS `players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `players` (
+  `player_id` int NOT NULL AUTO_INCREMENT,
+  `team_id` int DEFAULT NULL,
+  `name` varchar(100) NOT NULL,
+  `position` varchar(50) DEFAULT NULL,
+  `nationality` varchar(50) DEFAULT NULL,
+  `birthdate` date DEFAULT NULL,
+  PRIMARY KEY (`player_id`),
+  KEY `team_id` (`team_id`),
+  CONSTRAINT `players_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `teams` (`team_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `players`
+--
+
+LOCK TABLES `players` WRITE;
+/*!40000 ALTER TABLE `players` DISABLE KEYS */;
+/*!40000 ALTER TABLE `players` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teams`
+--
+
+DROP TABLE IF EXISTS `teams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teams` (
+  `team_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `stadium` varchar(100) DEFAULT NULL,
+  `founded_year` int DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`team_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teams`
+--
+
+LOCK TABLES `teams` WRITE;
+/*!40000 ALTER TABLE `teams` DISABLE KEYS */;
+INSERT INTO `teams` VALUES (1,'Manchester City','Etihad Stadium',1880,'Manchester'),(2,'Arsenal','Emirates Stadium',1886,'London'),(3,'Manchester United','Old Trafford',1878,'Manchester'),(4,'Newcastle United','St James\' Park',1892,'Newcastle'),(5,'Liverpool','Anfield',1892,'Liverpool'),(6,'Tottenham Hotspur','Tottenham Hotspur Stadium',1882,'London'),(7,'Chelsea','Stamford Bridge',1905,'London'),(8,'Brighton & Hove Albion','Amex Stadium',1901,'Brighton'),(9,'Aston Villa','Villa Park',1874,'Birmingham'),(10,'West Ham United','London Stadium',1895,'London'),(11,'Brentford','Brentford Community Stadium',1889,'Brentford'),(12,'Fulham','Craven Cottage',1879,'London'),(13,'Crystal Palace','Selhurst Park',1905,'London'),(14,'Wolverhampton Wanderers','Molineux Stadium',1877,'Wolverhampton'),(15,'Everton','Goodison Park',1878,'Liverpool'),(16,'Nottingham Forest','City Ground',1865,'Nottingham'),(17,'Bournemouth','Vitality Stadium',1899,'Bournemouth'),(18,'Leeds United','Elland Road',1919,'Leeds'),(19,'Burnley','Turf Moor',1882,'Burnley'),(20,'Sheffield United','Bramall Lane',1889,'Sheffield');
+/*!40000 ALTER TABLE `teams` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +108,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-08-05  2:23:57
+-- Dump completed on 2024-08-05 14:59:40
